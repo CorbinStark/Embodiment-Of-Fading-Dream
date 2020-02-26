98abf9d7ba8288fa1d447919dd7153314203f385
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 09:28:49 -0800

    Fixed map rendering

--- src/map.rs
+++ src/map.rs
@@ -6,6 +6,8 @@ const TILE_SIZE: i32 = 16;
 #[allow(dead_code)]
 pub struct Unit {
     player_owned: bool,
+    x: i32,
+    y: i32,
     health: i32,
     maxhealth: i32,
     damage: i32,
@@ -20,55 +22,39 @@ pub struct Map {
     grid: Vec<Vec<i32>>, //2d array (2d vec) of i32 (IDs) that correspond to tile types (textures for the tiles, wall, ground, etc.)
     width: i32,
     height: i32,
+    x: i32,
+    y: i32,
     units: Vec<Unit>,
-    tiles: Vec<Texture2D>, //the ID in grid[x][y] correlates to an image in this Vec. EG.: map.tiles[ map.grid[x][y] ] would access the tile image for the x y position on the map
+    tiles: Texture2D, //the ID in grid[x][y] correlates to an image in this Vec. EG.: map.tiles[ map.grid[x][y] ] would access the tile image for the x y position on the map
 }
 
 #[allow(dead_code)]
 impl Map {
-    pub fn create_blank(width: usize, height: usize) -> Self{
+    pub fn new(width: usize, height: usize, rl: &mut RaylibHandle, thread: &mut RaylibThread) -> Self{
         let mut grid: Vec<Vec<i32>> = vec![];
+        grid.resize(width, vec![]);
         for x in 0..width {
-            for y in 0..height {
-                grid[x][y] = 0; //init all to 0
-            }
+            grid[x].resize(height, 0);
         }
         Map {
             grid: grid,
             width: width as i32,
             height: height as i32,
+            x: 0,
+            y: 0,
             units: vec![],
-            tiles: vec![],
-        }
-    }
-    pub fn new(grid: Vec<Vec<i32>>,
-    width: i32,
-    height: i32,
-    units: Vec<Unit>,
-    tiles: Vec<Texture2D>) -> Self {
-        Map {
-            grid: grid,
-            width: width,
-            height: height,
-            units: units,
-            tiles: tiles,
+            tiles: rl.load_texture(thread, "art/Dungeon_Tileset.png").unwrap(),
         }
     }
     pub fn draw(&self, d: &mut RaylibDrawHandle) {
         for x in 0..self.width {
             for y in 0..self.height {
-                //doing the conversions to make everything a usize so that it can index is annoying.....
-                d.draw_texture(&self.tiles[self.grid[x as usize][y as usize] as usize], x * TILE_SIZE, y * TILE_SIZE, Color::WHITE);
+                //Used ID to determine x and y position of tile on the tileset.
+                let id = self.grid[x as usize][y as usize];
+                let source = Rectangle::new(((id % 10) * TILE_SIZE) as f32, ((id / 10) * TILE_SIZE) as f32, TILE_SIZE as f32, TILE_SIZE as f32);
+                let position = Vector2::new((x * TILE_SIZE) as f32, (y * TILE_SIZE) as f32);
+                d.draw_texture_rec(&self.tiles, source, position, Color::WHITE);
             }
-        }
-    }
-    pub fn copy(other: Map) -> Self {
-        Map {
-            grid: other.grid,
-            width: other.width,
-            height: other.height,
-            units: other.units,
-            tiles: other.tiles,
         }
     }
 }
@@ -101,15 +87,6 @@ fn add_fill_node(map: &Map, dx: i32, dy: i32, n: &FillNode, visited: &mut Vec<bo
     if n.depth >= range {
         return;
     }
-
-    //let mapcopy = Map {
-      //  grid: map.grid.clone(),
-      //  width: map.width,
-      //  height: map.height,
-      //  units: map.units.clone(),
-      //  tiles: map.tiles.clone(),
-    //};
-    //let h = heuristic(mapcopy, n.x, n.y);
     let h = heuristic(map.grid[n.x as usize][n.y as usize]);
     if h == -1 {
         return;
@@ -123,14 +100,17 @@ fn add_fill_node(map: &Map, dx: i32, dy: i32, n: &FillNode, visited: &mut Vec<bo
 }
 
 #[allow(dead_code)]
-fn floodfill(map: Map, start: (i32, i32), range: i32, heuristic: fn(i32) -> i32) -> Vec<(i32, i32)>{
+fn floodfill(map: Map, start: (i32, i32), range: i32, heuristic: fn(i32) -> i32) -> Vec<(i32, i32)> {
+    //Set up visited array
     let mut visited: Vec<bool> = vec![];
     visited.reserve( (map.width * map.height) as usize);
     visited[ (start.0 + start.1 * map.width) as usize ] = true;
 
+    //Set up queue
     let mut q: Vec<FillNode> = vec![];
     q.push( FillNode::new(start.0, start.1, 0) );
 
+    //set up result path
     let mut path: Vec<(i32, i32)> = vec![];
     path.push(start);
 
